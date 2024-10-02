5111eacc7bbcc80a9a11231201d37be435f41098
Maliha-Moon <malihamoon05@gmail.com>
2024-10-02 21:03:12 +0600

    Add richie_rich file

---
+++ richie_rich.cpp
@@ -0,0 +1,17 @@
+// 878
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int A, B, X;
+        cin >> A >> B >> X;
+        cout << (B - A) / X << "\n";
+    }
+    return 0;
+}
